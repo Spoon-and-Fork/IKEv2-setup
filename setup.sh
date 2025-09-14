81435f50f712ac6fd95fe2277eb26c96feb5a313
ycdwell <overlord.7711@gmail.com>
2025-09-14 13:34:23 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -65,7 +65,7 @@ debconf-set-selections <<< "postfix postfix/mailname string $HOSTNAME"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 
 apt-get -o Acquire::ForceIPv4=true install -y \
-  language-pack-en iptables-persistent postfix mutt unattended-upgrades certbot nginx uuid-runtime \
+  language-pack-en iptables-persistent postfix mutt unattended-upgrades certbot nginx uuid-runtime nfs-common \
   strongswan libstrongswan-standard-plugins strongswan-libcharon libcharon-extra-plugins
 
 # in 22.04 libcharon-standard-plugins is replaced with libcharon-extauth-plugins
@@ -688,5 +688,6 @@ echo "Connection instructions have been emailed to you, and can also be found in
 # necessary for IKEv2?
 # Windows: https://support.microsoft.com/en-us/kb/926179
 # HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\PolicyAgent += AssumeUDPEncapsulationContextOnSendRule, DWORD = 2
+
 
 
